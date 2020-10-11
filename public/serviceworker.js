cd89bfb8fc925b59be49247c5b547bf007304c10
Daniela Alarcón <daniela.alarcongamboa27@gmail.com>
2020-10-11 12:46:35 -0600

    offline commit

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,7 +17,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match(["/offline.html", "/images/logo.png"]));
+      return fetch(event.request).catch(() => caches.match("/offline.html"));
     })
   );
 });
