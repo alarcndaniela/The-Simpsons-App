87d4fdca5b815d5613f569f3a49942d2de1738ea
Daniela Alarcón <daniela.alarcongamboa27@gmail.com>
2020-10-11 12:48:28 -0600

    offline commit

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ["/index.html", "/offline.html","/images/logo.png"];
+const urlsToCache = ["index.html", "offline.html"];
+
 const self = this;
 
 // Install SW
@@ -17,7 +18,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("/offline.html"));
+      return fetch(event.request).catch(() => caches.match("offline.html"));
     })
   );
 });
